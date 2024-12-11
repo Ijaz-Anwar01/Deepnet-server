f1ac25c55914cd599959dbcbad463a31b21cd6cb
Ijaz Anwar <75467415+Ijaz-Anwar01@users.noreply.github.com>
2024-12-11 23:36:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,18 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({origin: 'https://deepnet-client.onrender.com'}));
+app.use(cors({
+  origin: 'https://deepnet-client.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true,
+}));
+
 app.use(express.json());
 
-// MongoDB connection
+
 mongoose.connect('mongodb+srv://ijasanwar1:bukSN52k2X1a3ko0@cluster0.qe0m6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',)
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.log('MongoDB connection error:', error));
 
-// Routes
+
 app.use('/api', menuRoutes);
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
